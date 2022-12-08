2f6af076f7556b69593babcca614710cec218cd9
xenkiisj <xenkiskijump@gmail.com>
2022-12-08 20:08:30 +0100

    lekcjalisty

---
+++ listy.py
@@ -0,0 +1,28 @@
+'''
+x1 = int(input('podaj liczbe: '))
+x2 = int(input('podaj liczbe: '))
+x12 = (x1 + x2) / 2
+print(round(x12))
+'''
+
+'''
+wi = True
+proba = 0
+suma = 0
+while wi:
+    ocena = int(input("podaj ocene: "))
+    proba+=1
+    suma+=ocena
+    k = input('czy masz wiecej?[t/n]: ')
+    if (k == 'n' or k == 'no'):
+        wi = False
+else:
+    print(f"twoja srednia to: {suma/proba}")
+'''
+lista = [pies]
+print(len[lista])
+print(lista(len[lista]-1))
+
+# 1 tworzy liste 15 elementow, w tym co najmniej 2 innych list po 3 elementy (hardcoded)
+# 2 wypisze pierwszy, ostatni i srodkowy element listy
+# 3* wypisze ostatni element z kazdej z 2 podlist
--- loops.py
+++ loops.py
@@ -1,13 +1,9 @@
-l = input("podaj liczbe linijek: ")
+lin = int(input("podaj liczbe linijek: "))
 x = 1
-while x<=int(l):
-    gw = '*'*x
-    print(gw)
-    x+=1
-
-    
-
-
-
-
-
+s = int(lin)
+while x<=(int(lin)*2):
+    print(" "*s + ('*'*x))
+    x+=2
+    s-=1
+else:
+    print(lin*' '+'*')
